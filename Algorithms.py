a731ae5754824746ef4db5b9fb4ee79b4dfe7e97
Erik Gule <erik.robby@hotmail.com>
2018-09-14 04:21:08 +0200

    It now plots the time duration

--- Algorithms.py
+++ Algorithms.py
@@ -1,15 +1,17 @@
 from random import *
 from time import *
-#Program that compares the time usage
-#of bolo-sort and insertion-sort
-seed()
-oldX = []
-count= 0
-bogoCount = 0
+import numpy as np
+import matplotlib.pyplot as plt
 
-def randomize():
-    for i in range(0, 1000):
-        oldX.append(randint(0, 100))
+seed()
+x = []
+count = 0
+
+
+def randomize(size):
+    for i in range(0, size):
+        x.append(randint(0, 100))
+
 
 def inorder(x):
     i = 0
@@ -20,46 +22,86 @@ def inorder(x):
         i += 1
     return True
 
+
 def bogo(x):
-    global bogoCount
     while not inorder(x):
-        bogoCount+=1
+        countN()
         shuffle(x)
     return x
 
 
 def insertionsort(x):
-    global count
-    for j in range(1,len(x)):
+    for j in range(1, len(x)):
         key = x[j]
-        i = j-1
-        count+=1
+        i = j - 1
+        countN()
         while (i > -1) and key < x[i]:
-            count+=1
-            x[i+1]=x[i]
-            i=i-1
-        x[i+1] = key
+            countN()
+            x[i + 1] = x[i]
+            i = i - 1
+        x[i + 1] = key
     return x
 
+def countN():
+    global count
+    count = count +1
 
+def runBogo(size):
+    global x
+    global count
+    x.clear()
+    randomize(size)
+    count = 0
+    print("\nBogo Sort (", size, "):")
+    print("Before: ", x)
+    startBogo = time()
+    x = bogo(x)
+    duration = time() - startBogo
+    print("After: ", x)
+    print("%.2f seconds" % (duration))
+    print("Count", count)
+    return duration
 
+def runInsertion(size):
+    global x
+    global count
+    x.clear()
+    randomize(size)
+    count = 0
+    print("\nInsertion Sort (", size, "):")
+    print("Before: ", x)
+    startInsertion = time()
+    x = insertionsort(x)
+    duration = time() - startInsertion
+    print("After: ", x)
+    print("%.2f seconds" % (duration))
+    print("Count:", count)
+    return duration
 
+insertionDurations = []
+insertionSize = []
+bogoDurations = []
+bogoSize = []
 
-randomize()
-startInsertion = time()
-print("\nInsertion Sort:")
-print("Before: ", oldX)
-y = insertionsort(oldX)
-print("After: ", y)
-print("%.2f seconds" % (time() - startInsertion))
-print("Count:", count)
+for size in range(1, 1000):
+    insertionDurations.append(runInsertion(size))
+    insertionSize.append(size)
 
-oldX = []
-randomize()
-startBogo = time()
-print("\nBogo Sort:")
-print("Before: ", oldX)
-x = bogo(oldX)
-print("After: ", x)
-print("%.2f seconds" % (time() - startBogo))
-print("Count", bogoCount)
+print("Insertion times:", insertionDurations)
+plt.plot(insertionSize, insertionDurations)
+plt.title("Insertion-sort")
+plt.xlabel("Number of elements")
+plt.ylabel("Time in seconds")
+
+for size in range(1, 11):
+    bogoDurations.append(runBogo(size))
+    bogoSize.append(size)
+
+print("Bogo times", bogoDurations)
+
+plt.figure(2)
+plt.plot(bogoSize, bogoDurations)
+plt.title("Bogo-sort")
+plt.xlabel("Number of elements")
+plt.ylabel("Time in seconds")
+plt.show()
